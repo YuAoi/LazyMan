b645d48b8994bbb4ad6808ce6eed6b62249d2f68
z <zibo.z.liu@gmail.com>
2017-02-19 23:33:27 +0800

    Create LazyMan.js

---
+++ LazyMan.js
@@ -0,0 +1,37 @@
+class LazyMan {
+  constructor(name) {
+    this.name = name
+    this.task = []
+    Promise.resolve().then(() => this.run())
+  }
+
+  async run() {
+    for (let fn of this.task) await fn()
+    return this
+  }
+
+  eat(food) {
+    this.use(() => {
+      console.log(`eat: ${food}`)
+    })
+    return this
+  }
+
+  sleep(ms) {
+    this.use(() => {
+      console.log(`sleep:${ms}`)
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('wakeup')
+            resolve()
+        }, ms)
+      })
+    })
+    return this
+  }
+
+  use(fn) {
+    this.task.push(fn)
+    return this
+  }
+}

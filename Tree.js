41e22975565005cc73cc5d05c12fa64a4203f6fe
YuAoi <zibo.z.liu@gmail.com>
2017-04-14 18:57:27 +0800

    Tree.js

--- Tree.js
+++ Tree.js
@@ -8,8 +8,10 @@ class Node {
   show () {
     return this.data
   }
+}
 
-  BST () {
+class BST {
+  constructor() {
     this.root = null
   }
 
@@ -38,4 +40,86 @@ class Node {
       }
     }
   }
+
+  inOrder (node) {
+    if (!(node === null)) {
+      this.inOrder(node.left)
+      putstr(node.show() + ' ')
+      this.inOrder(node.right)
+    }
+  }
+
+  preOrder (node) {
+    if (!(node === null)) {
+      putstr(node.show() + ' ')
+      this.preOrder(node.left)
+      this.preOrder(node.right)
+    }
+  }
+
+  postOrder (node) {
+    if (!(node === null)) {
+      this.postOrder(node.left)
+      this.postOrder(node.right)
+      putstr(node.show() + ' ')
+    }
+  }
+
+  getMin () {
+    let current = this.root
+    while (!(current.left === null)) {
+      console.log('process', current.data)
+      current = current.left
+    }
+    return current.data
+  }
+
+  getMax () {
+    let current = this.root
+    while (!(current.right === null)) {
+      current = current.right
+    }
+    return current.data
+  }
+
+  find (data) {
+    let current = this.root
+    while (current !== null) {
+      if (current.data === data) {
+        return current
+      } else if (data < current.data) {
+        current = current.left
+      } else {
+        current = current.right
+      }
+    }
+    return null
+  }
 }
+
+function putstr (str) {
+  console.log(str)
+}
+
+let nums = new BST()
+nums.insert(23)
+nums.insert(45)
+nums.insert(16)
+nums.insert(37)
+nums.insert(3)
+nums.insert(99)
+nums.insert(9)
+nums.insert(12)
+nums.insert(52)
+nums.insert(2)
+// console.log('Inorder traversal: ')
+// nums.inOrder(nums.root)
+// console.log('Preorder traversal: ')
+// nums.preOrder(nums.root)
+// console.log('PostOrder traversal: ')
+// nums.postOrder(nums.root)
+
+console.log('Min', nums.getMin())
+console.log('Max', nums.getMax())
+console.log('99', nums.find(99))
+// console.log(JSON.stringify(nums, null, 2))
